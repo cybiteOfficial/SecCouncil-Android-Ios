f5f441ca0aa240e3247d9873f914ddc1932432a8
Arjun <niteshkrjhag@gmail.com>
2025-01-02 12:11:43 +0545

    Navigation included

---
+++ app/src/main/java/com/example/seccouncil/navigation/Navigation.kt
@@ -0,0 +1,140 @@
+package com.example.seccouncil.navigation
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.example.seccouncil.screens.Login
+import com.example.seccouncil.screens.NotificationScreen
+import com.example.seccouncil.screens.Otp
+import com.example.seccouncil.screens.PasswordScreen
+import com.example.seccouncil.screens.SearchScreen
+import com.example.seccouncil.screens.SignUp
+import com.example.seccouncil.screens.homescreen.CourseScreen
+import com.example.seccouncil.screens.homescreen.HomeScreen
+import com.example.seccouncil.screens.payment.PaymentScreen
+import com.example.seccouncil.screens.profilesetting.Certification
+import com.example.seccouncil.screens.profilesetting.FavoritesScreenPs
+import com.example.seccouncil.screens.profilesetting.HelpCenter
+import com.example.seccouncil.screens.profilesetting.ProfileScreen
+import com.example.seccouncil.screens.profilesetting.ProfileSettingScreen
+import com.example.seccouncil.screens.profilesetting.Security
+
+@Composable
+fun Navigation(
+    navController:NavHostController = rememberNavController()
+){
+    val backStackEntry by navController.currentBackStackEntryAsState()
+    val currentScreen = Routes.Signup
+
+    NavHost(
+        navController = navController,
+        startDestination = Routes.Signup.name,
+        modifier = Modifier
+    ){
+        composable(
+            route = Routes.Signup.name
+        ){
+            SignUp(
+                onLoginClick = {navController.navigate(Routes.Login.name)}
+            )
+        }
+        composable(
+            route = Routes.Login.name
+        ){
+            Login(
+                onLoginClicked = {navController.navigate(Routes.Otp.name)}
+            )
+        }
+        composable(
+            route = Routes.Otp.name
+        ){
+            Otp(
+                onVerifyClicked = {navController.navigate(Routes.Password.name)}
+            )
+        }
+        composable(
+            route = Routes.Password.name
+        ){
+            PasswordScreen(
+                onLoginClicked = {navController.navigate(Routes.Home.name)}
+            )
+        }
+        composable(route = Routes.Home.name){
+            HomeScreen(
+                onCertificationClicked = {navController.navigate(Routes.Certification.name)},
+                onCourseClicked = {navController.navigate(Routes.Courses.name)},
+                onProfileClicked = {navController.navigate(Routes.Profile.name)},
+                onSearchClicked = {navController.navigate(Routes.Search.name)},
+                onBellClicked = {navController.navigate(Routes.Notification.name)},
+                onHomeClicked = {navController.navigate(Routes.Home.name)},
+                onProfileSettingClicked = {navController.navigate(Routes.ProfileSetting.name)}
+            )
+        }
+        composable(route = Routes.Courses.name){
+            CourseScreen(
+                onEnrollClicked = {
+                    navController.navigate(Routes.Payment.name)
+                },
+                onBackClicked = {navController.navigateUp()}
+            )
+        }
+        composable(route = Routes.Payment.name){
+            PaymentScreen(
+                onBackClicked = {navController.navigateUp()}
+            )
+        }
+        composable(route = Routes.Profile.name){
+            ProfileScreen(
+                onBackClicked = {navController.navigateUp()},
+                onProfileClicked = {navController.navigate(Routes.ProfileSetting.name)},
+                onFavouritesClicked = {navController.navigate(Routes.Favourites.name)},
+                onSecurityClicked = {navController.navigate(Routes.Security.name)},
+                onHelpCenterClicked = {navController.navigate(Routes.HelpCenter.name)},
+                onCertificationsClicked = {navController.navigate(Routes.Certification.name)}
+            )
+        }
+        composable(route = Routes.ProfileSetting.name){
+            ProfileSettingScreen(
+                onBackClicked = {navController.navigateUp()}
+            )
+        }
+        composable(route = Routes.Notification.name){
+            NotificationScreen(
+                onBackClicked = {navController.navigateUp()}
+            )
+        }
+        composable(route = Routes.Search.name){
+            SearchScreen(
+                onBackClicked = {navController.navigateUp()}
+            )
+        }
+        composable(route = Routes.Favourites.name){
+            FavoritesScreenPs(
+                onBackClicked = {navController.navigateUp()}
+            )
+        }
+        composable(route = Routes.Security.name){
+           Security(
+               onBackClicked = {navController.navigateUp()}
+           )
+        }
+
+        composable(route = Routes.HelpCenter.name){
+          HelpCenter(
+              onBackClicked = {navController.navigateUp()}
+          )
+        }
+        composable(route = Routes.Certification.name){
+            Certification(
+                onBackClicked = {navController.navigateUp()}
+            )
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/seccouncil/navigation/Routes.kt
@@ -0,0 +1,19 @@
+package com.example.seccouncil.navigation
+
+enum class Routes {
+    Signup,
+    Login,
+    Otp,
+    Password,
+    Home,
+    Notification,
+    Search,
+    Courses,
+    Payment,
+    Profile,
+    ProfileSetting,
+    Favourites,
+    Security,
+    HelpCenter,
+    Certification
+}
