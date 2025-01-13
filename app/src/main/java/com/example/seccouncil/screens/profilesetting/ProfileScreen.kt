3f63ce82c8ec3d65ce0db1938f78fb63de2fe4f0
Arjun <niteshkrjhag@gmail.com>
2025-01-13 11:33:46 +0545

    Profile Screen done

--- app/src/main/java/com/example/seccouncil/screens/profilesetting/ProfileScreen.kt
+++ app/src/main/java/com/example/seccouncil/screens/profilesetting/ProfileScreen.kt
@@ -13,28 +13,39 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicText
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.seccouncil.R
-import com.example.seccouncil.common.ProfileScreenItem
+import com.example.seccouncil.common.TextComm
+import com.example.seccouncil.ui.theme.urbanist
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview(showBackground = true, showSystemUi = true)
@@ -62,8 +73,15 @@ fun ProfileScreen(
         modifier = Modifier
             .fillMaxSize()
         ,
+        containerColor = Color.White,
         topBar = {
             TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = Color.White, // Set the background color of the TopAppBar
+                    titleContentColor = Color.Black, // Color for the title
+                    actionIconContentColor = Color.Black, // Color for the action icons like bell and profile
+                    navigationIconContentColor = Color.Black // Color for the navigation icon, if any
+                ),
                 title = {
                     Row(
                         modifier = Modifier.fillMaxWidth()
@@ -109,7 +127,7 @@ fun ProfileScreen(
             ) {
                 Box(
                     modifier = Modifier
-                        .size(100.dp)
+                        .size(120.dp)
                         .border(0.5.dp, color = Color.White,RoundedCornerShape(10.dp))
                         .clip(RoundedCornerShape(10.dp))
                 ){
@@ -122,37 +140,92 @@ fun ProfileScreen(
                 Spacer(Modifier.height(5.dp))
                 Text(
                     text = "Alex Jackson",
-                    fontWeight = FontWeight.SemiBold,
-                    fontFamily = FontFamily.Monospace,
-                    fontSize = 16.sp
+                    style = TextStyle(
+                        fontFamily = urbanist,
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold
+                    )
                 )
                 Text(
                     text = "alexj1234@gmail.com",
                     fontFamily = FontFamily.Monospace,
-                    fontSize = 12.sp,
+                    fontSize = 16.sp,
                     color = Color.Gray
 
                 )
             }
-            Spacer(Modifier.height(5.dp))
-            profileOptions.forEachIndexed{
-                index,profileoption->
-                ProfileScreenItem(
-                    image = iconOptions[index],
-                    title = profileoption,
-                    onClicked = {
-                        when(index){
-                            0->onProfileClicked()
-                            1->onFavouritesClicked()
-                            2->onSecurityClicked()
-                            3->onCertificationsClicked()
-                            5->onHelpCenterClicked()
-                        }
-                    }
-                )
-            }
+
+           Button(
+               onClick = {},
+               modifier = Modifier
+                   .fillMaxWidth(),
+               colors = ButtonColors(
+                   containerColor = Color.Black,
+                   disabledContentColor = Color.White,
+                   contentColor = Color.White,
+                   disabledContainerColor = Color.Black
+               ),
+               shape = RoundedCornerShape(10.dp)
+
+           ) {
+               Text(
+                   "Edit Profile",
+                   style = TextStyle(
+                       fontFamily = urbanist
+                   )
+               )
+           }
+            Spacer(Modifier.height(10.dp))
+            TextComm(
+                text = "Your Learning Process",
+                fontWeight = FontWeight.Bold,
+                modifier = modifier.align(Alignment.Start)
+            )
+            ProfileContent()
+            ProfileContent(
+                title = "Minutes watched",
+                number = "1400"
+            )
         }
     }
 
 }
+
+@Composable
+private fun ProfileContent(
+    modifier: Modifier = Modifier,
+    title:String = "Course Completed",
+    number:String = "2",
+){
+    Column(
+        modifier = modifier.fillMaxWidth()
+            .padding(top = 5.dp),
+        horizontalAlignment = Alignment.Start
+    ){
+        TextComm(
+            text = title,
+            fontWeight = FontWeight.Medium
+        )
+        Spacer(Modifier.height(16.dp))
+        TextComm(
+            text = number,
+            fontWeight = FontWeight.Normal,
+            fontSize = 32.sp
+        )
+        Spacer(Modifier.height(8.dp))
+        BasicText(
+            text = buildAnnotatedString {
+                withStyle(style = SpanStyle(color = Color.Gray)) {
+                    append("last 30 days ")
+                }
+                withStyle(style = SpanStyle(color = colorResource(R.color.increment), fontWeight = FontWeight.Bold)) {
+                    append(" +100%")
+                }
+            },
+            style = TextStyle(fontSize = 16.sp, fontFamily = urbanist)
+        )
+    }
+
+}
+
 
