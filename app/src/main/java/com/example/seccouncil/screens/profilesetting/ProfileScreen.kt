1f83311f67f0d332daf3d0374444c65ba9086b6e
Arjun <niteshkrjhag@gmail.com>
2025-02-08 14:59:26 +0545

    update profile pic funcitionlity added

--- app/src/main/java/com/example/seccouncil/screens/profilesetting/ProfileScreen.kt
+++ app/src/main/java/com/example/seccouncil/screens/profilesetting/ProfileScreen.kt
@@ -24,17 +24,13 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -52,12 +48,11 @@ import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import coil3.compose.rememberAsyncImagePainter
+import coil.compose.rememberAsyncImagePainter
 import com.example.seccouncil.R
 import com.example.seccouncil.common.TextComm
 import com.example.seccouncil.screens.homescreen.HomescreenViewmodel
 import com.example.seccouncil.ui.theme.urbanist
-import com.example.seccouncil.utlis.DataStoreManger
 import kotlinx.coroutines.CoroutineScope
 import java.io.File
 
