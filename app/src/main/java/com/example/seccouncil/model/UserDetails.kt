2c08cf53f5416673808064f477b11b1da0ccf928
Arjun <niteshkrjhag@gmail.com>
2025-05-05 16:54:15 +0530

    added more parameters in userdetails

--- app/src/main/java/com/example/seccouncil/model/UserDetails.kt
+++ app/src/main/java/com/example/seccouncil/model/UserDetails.kt
@@ -1,11 +1,14 @@
 package com.example.seccouncil.model
 
 data class UserDetails(
-val emailAddress: String = "",
-val mobileNumber: String = "",
-val password: String = "",
-val name: String = "",
-)
+    val emailAddress: String = "",
+    val mobileNumber: String = "",
+    val password: String = "",
+    val name: String = "",
+    val dob: String = "",
+    val gender: String = "",
+    val about: String = ""
+    )
 
 data class UserProfilePhoto(
     val photo: String
